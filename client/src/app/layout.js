7cf4e15a129152e9c53562a65ffb871e4188dea7
Chanbin Na <chanbinna@gmail.com>
2024-09-14 14:13:14 +0900

    ddd

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
