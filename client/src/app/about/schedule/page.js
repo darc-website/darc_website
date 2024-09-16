aae09e5b7d7a1b028e28c1b1148d15f28ce73bde
Chanbin Na <chanbinna@gmail.com>
2024-09-16 18:27:09 +0900

    slight change

--- client/src/app/about/schedule/page.js
+++ client/src/app/about/schedule/page.js
@@ -48,6 +48,317 @@ export default function Schedule() {
                                 </>
                             )}
                         </TransformWrapper>
+
+                        {/* <div className={styles.row}>
+                            <div className={styles.blankBlock}>
+                            </div>
+                            <div className={styles.dayBlock}>
+                                <p>월요일</p>
+                            </div>
+                            <div className={styles.dayBlock}>
+                                <p>화요일</p>
+                            </div>
+                            <div className={styles.dayBlock}>
+                                <p>수요일</p>
+                            </div>
+                            <div className={styles.dayBlock}>
+                                <p>목요일</p>
+                            </div>
+                            <div className={styles.dayBlock}>
+                                <p>금요일</p>
+                            </div>
+                            <div className={styles.dayBlock}>
+                                <p>토요일</p>
+                            </div>
+                        </div>
+                        <div className={styles.row}>
+                            <div className={styles.timeBlock}>
+                                <p>8:00 ~ 9:30
+                                </p>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR1}>
+                                    <p>기상 & 아침식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR1}>
+                                    <p>기상 & 아침식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR1}>
+                                    <p>기상 & 아침식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR1}>
+                                    <p>기상 & 아침식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR1}>
+                                    <p>기상 & 아침식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR1}>
+                                    <p>기상 & 아침식사</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.row}>
+                            <div className={styles.timeBlock}>
+                                <p>9:30 ~ 12:00
+                                </p>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR2}>
+                                    <p>인지 행동 치료</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR2}>
+                                    <p>12단계 촉진 치료</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR2}>
+                                    <p>동기 강화 치료</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR2}>
+                                    <p>12단계 촉진 치료</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR2}>
+                                    <p>인지 행동 치료</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR2_2}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.row}>
+                            <div className={styles.timeBlock}>
+                                <p>12:00 ~ 14:00
+                                </p>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR3}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR3}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR3}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR3}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR3}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR3}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.row}>
+                            <div className={styles.timeBlock}>
+                                <p>14:00 ~ 15:00
+                                </p>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR4}>
+                                    <p>점심 식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR4}>
+                                    <p>점심 식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR4}>
+                                    <p>점심 식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR4}>
+                                    <p>점심 식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR4}>
+                                    <p>점심 식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR4}>
+                                    <p>점심 식사</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.row}>
+                            <div className={styles.timeBlock}>
+                                <p>15:00 ~ 17:00
+                                </p>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR5}>
+                                    <p>약물 인식 교육</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR5}>
+                                    <p>영성 프로그램</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR5}>
+                                    <p>미술 프로그램</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR5}>
+                                    <p>독서 프로그램</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR5}>
+                                    <p>연극 프로그램</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR5_2}>
+                                    <p>다르크 가족참여 <br />자조모임</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.row}>
+                            <div className={styles.timeBlock}>
+                                <p>17:00 ~ 19:00
+                                </p>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR6}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR6}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR6}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR6}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR6}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR6}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.row}>
+                            <div className={styles.timeBlock}>
+                                <p>19:00 ~ 20:00
+                                </p>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR7}>
+                                    <p>자유 <br />NA 모임</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR7}>
+                                    <p>학동 <br />NA 모임</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR7_2}>
+                                    <p>개인 정비 시간</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR7}>
+                                    <p>온유 <br />NA 모임</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR7}>
+                                    <p>가정동 <br />NA 모임</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR7}>
+                                    <p>치유 <br />NA 모임</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.row}>
+                            <div className={styles.timeBlock}>
+                                <p>19:00 ~ 20:00
+                                </p>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR8}>
+                                    <p>저녁식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR8}>
+                                    <p>저녁식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR8}>
+                                    <p>저녁식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR8}>
+                                    <p>다르크 <br />주간회의 <br />/저녁식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR8}>
+                                    <p>저녁식사</p>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleBlock}>
+                                <div className={styles.textBlockR8}>
+                                    <p>저녁식사</p>
+                                </div>
+                            </div>
+                        </div> */}
                     </div>
 
                     <div className={styles.bottom}>
