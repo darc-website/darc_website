2d81513c5613938005207548b971df4d2bacd879
7174Andy <hep003@ucsd.edu>
2024-12-31 20:31:54 +0900

    add pagination

--- client/components/NoticeAdmin.js
+++ client/components/NoticeAdmin.js
@@ -30,15 +30,48 @@ const data = [
     date: new Date("2021-10-01"),
     content: "공고 내용 1",
   },
+  {
+    title: "공지사항 5",
+    category: "서비스",
+    date: new Date("2021-10-01"),
+    content: "서비스 내용 1",
+  },
+  {
+    title: "공지사항 5",
+    category: "서비스",
+    date: new Date("2021-10-01"),
+    content: "서비스 내용 1",
+  },
+  {
+    title: "공지사항 5",
+    category: "서비스",
+    date: new Date("2021-10-01"),
+    content: "서비스 내용 1",
+  },
 ];
 
 const NoticeAdmin = () => {
   const [currTab, setCurrTab] = useState("전체");
+  const [currentPage, setCurrentPage] = useState(1);
+  const noticesPerPage = 5;
 
   const filteredData =
     currTab === "전체"
       ? data
       : data.filter((notice) => notice.category === currTab);
+
+  const totalPages = Math.ceil(filteredData.length / noticesPerPage);
+  const startIndex = (currentPage - 1) * noticesPerPage;
+  const paginatedNotices = filteredData.slice(
+    startIndex,
+    startIndex + noticesPerPage
+  );
+
+  const handlePageChange = (page) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
 
   return (
     <div className={styles.noticeContainer}>
@@ -106,10 +139,29 @@ const NoticeAdmin = () => {
         </div>
       </div>
       <ul className={styles.noticeList}>
-        {filteredData.map((notice, index) => (
+        {paginatedNotices.map((notice, index) => (
           <SingleNotice key={index} notice={notice} />
         ))}
       </ul>
+      <div className={styles.paginationContainer}>
+        <button className={styles.paginationButton}>
+          <i className="pi pi-chevron-left" style={{ color: "black" }} />
+        </button>
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button
+            key={i}
+            onClick={() => handlePageChange(i + 1)}
+            className={`${styles.paginationButton} ${
+              currentPage === i + 1 ? styles.active : ""
+            }`}
+          >
+            {i + 1}
+          </button>
+        ))}
+        <button className={styles.paginationButton}>
+          <i className="pi pi-chevron-right" style={{ color: "black" }} />
+        </button>
+      </div>
     </div>
   );
 };
