6e6b3203e6ba1929113999c8ba7a3dfe2dec1353
7174Andy <hep003@ucsd.edu>
2025-06-16 13:27:42 -0700

    add announcement page for clients

--- client/src/app/contact/announcement/page.js
+++ client/src/app/contact/announcement/page.js
@@ -1,5 +1,5 @@
-'use client'
-import React from 'react';
+"use client";
+import React from "react";
 import Navigation from "../../../../components/navigation";
 import styles from "./resources.module.css";
 import Link from "next/link";
@@ -7,32 +7,35 @@ import { FaInstagram } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaTelegramPlane } from "react-icons/fa";
-import Footer from '../../../../components/footer';
-import ScrollToTop from '../../../../components/ScrollToTop';
+import NoticeAdmin from "../../../../components/NoticeAdmin";
+import Footer from "../../../../components/footer";
+import ScrollToTop from "../../../../components/ScrollToTop";
 
-
-export default function Mission() {
-    return (
-        <div className={styles['main-container']}>
-            <main className={styles.main}>
-                <ScrollToTop />
-                <Navigation />
-                <div className={styles.content}>
-                    <div className={styles.header}>
-                        <div className={styles.title}>
-                            <h1>공지사항.</h1>
-                        </div>
-                        <div className={styles.link}>
-                            <Link href="/contact/resources">Contact</Link>
-                            <p>&gt;</p>
-                            <Link href="/contact/resources"><h4>공지사항.</h4></Link>
-                        </div>
-                    </div>
-
-
-                </div>
-                <Footer />
-            </main>
+export default function Announcement() {
+  return (
+    <div className={styles["main-container"]}>
+      <main className={styles.main}>
+        <ScrollToTop />
+        <Navigation />
+        <div className={styles.content}>
+          <div className={styles.header}>
+            <div className={styles.title}>
+              <h1>공지사항.</h1>
+            </div>
+            <div className={styles.link}>
+              <Link href="/contact/resources">Contact</Link>
+              <p>&gt;</p>
+              <Link href="/contact/announcement">
+                <h4>공지사항.</h4>
+              </Link>
+            </div>
+          </div>
+          <div className={styles.noticeWrapper}>
+            <NoticeAdmin />
+          </div>
         </div>
-    );
+        <Footer />
+      </main>
+    </div>
+  );
 }
